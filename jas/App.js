d0a6ebc0327cda58db1079c2ec8a9bc589a85e18
jaschopp99 <jas.schopp99@gmail.com>
2020-03-26 13:53:19 -0400

    Updated NavBar

--- jas/App.js
+++ jas/App.js
@@ -1,45 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.Green}>Welcome Collector of things!</Text>
-      <Text style={styles.Green}>Would you like to...</Text>
-      <View style={styles.ButtonStyle}>
-        <Button title="Add to Your Collection"></Button>
-        <Button title="Start a New Collection"></Button>
-      </View>
-      <View>
-        <Text>Copyrighted by JAS Inc. 2020-2020©</Text>
-      </View>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
-   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#101820FF',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 100,
+    justifyContent: 'center',
   },
-  Green: {
-    color: '#006B38FF',
-    fontSize: 30,
-  },
-  ButtonStyle: {
-    flex:.15,
-    flexDirection: 'row',
-    width: '30%',
-     
-    justifyContent:'center',
-    alignContent:'center',
-  },
-  CopyRight: {
-    fontSize: 5,
-    
-  }
 });
