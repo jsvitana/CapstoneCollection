68a24949176a4d5812cfa0078adc7a450da6f742
jsvitana <51210786+jsvitana@users.noreply.github.com>
2020-03-26 13:49:05 -0400

    Bottom Navigation Added

--- jas/components/Home.js
+++ jas/components/Home.js
@@ -1,29 +1,20 @@
 import React from "react";
-import {View,Text,Button, StyleSheet} from "react-native";
-import {withNavigation} from 'react-navigation';
-import 'react-native-gesture-handler';    
+import {View,Text,Button, StyleSheet} from "react-native";   
 
 export default class Home extends React.Component {
-    static navigationOptions = {
-        title:"Home",
-    }
-
-    constructor(props) {
-        super(props);
-    }
-
-    changeView() {
-        const {navigation} = this.props;
-
-        navigation.navigate("test")
-    }
-
     render() {
         return(
-            <View>
-                <Text>Does this workdddkd</Text>
-                <Button title="Test" onPress={() => this.changeView()}></Button>
+            <View style={styles.container}>
+                <Text>Does this workdhhhhddkd</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+  });
