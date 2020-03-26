e95078c5967ab771fae24c55bc4bc320c1304b2e
jaschopp99 <jas.schopp99@gmail.com>
2020-03-26 13:10:08 -0400

    Update App.js

--- jas/App.js
+++ jas/App.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Green}>Welcome Collector of things!</Text>
+      <Text style={styles.Green}>Would you like to...</Text>
+      <View style={styles.ButtonStyle}>
+        <Button title="Add to Your Collection"></Button>
+        <Button title="Start a New Collection"></Button>
+      </View>
+      <View>
+        <Text>Copyrighted by JAS Inc. 2020-2020©</Text>
+      </View>
     </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#101820FF',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 100,
   },
+  Green: {
+    color: '#006B38FF',
+    fontSize: 30,
+  },
+  ButtonStyle: {
+    flex:.15,
+    flexDirection: 'row',
+    width: '30%',
+     
+    justifyContent:'center',
+    alignContent:'center',
+  },
+  CopyRight: {
+    fontSize: 5,
+    
+  }
 });
