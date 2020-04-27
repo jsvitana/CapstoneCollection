2c58bc4383cc209fb6068209e96e2054890f2e8e
joshua-stevens <60744415+joshua-stevens@users.noreply.github.com>
2020-04-27 13:47:56 -0400

    Working on Flat List

--- jas/components/collections.js
+++ jas/components/collections.js
@@ -1,25 +1,94 @@
 import React from "react";
-import {View,Text, StyleSheet} from "react-native";   
+import {View,Text, StyleSheet, FlatList, TouchableOpacity, ActivityIndicator} from "react-native";   
 import style from "./../styles/styles.json"
 import API from "./../API/APICalls.js"
 import { getLightEstimationEnabled } from "expo/build/AR";
 
 export default class test extends React.Component {
- 
-
-
- 
 
 
 
+    constructor() {
+        super();
+    
+        this.state = {
+          
+            dataSource: [],
+            loading: true
+        }
+      }
 
+
+    async componentDidMount() {
+        var api = new API();
+        var item = await api.GetItems(); 
+        console.log(item);
+        
+        this.setState({
+            dataSource: item,
+            loading: false
+
+        })
+        
+    }
+
+    FlatListItemSeperator = () => {
+        return (
+            <View style={{
+                height: .5,
+                width:"100%",
+                backgroundColor:"rgba(0,0,0,0.5)",
+            }}
+            />
+        );
+    }
+
+    renderItem=(data)=>
+    
+    <TouchableOpacity style={styles.list}>
+    <Text>{data.item.ItemName}</Text>
+    <Text>{data.item.UPC}</Text>
+    </TouchableOpacity>
     render() {
+        if(this.state.loading){
+            return(
+                <View style={styles.loader}>
+                    <ActivityIndicator size="large" color="#0c9"/>
+                </View>
+            )
+        }
+
         return(
-            <View style={{backgroundColor:style.backgroundColor, flex:1 , alignItems:"center" , justifyContent: 'flex-start' }}>
-                <Text style={{color:style.color, fontSize: 30, top:50}}>Collections</Text>
-                <Text>test</Text>
-                <Text>test 2</Text>
+            <View style={styles.container}>
+                <FlatList
+
+                    data= {this.state.dataSource}
+                    ItemSeparatorComponent = {this.FlatListItemSeperator}
+                    renderItem= {item=> this.renderItem(item)}
+                    keyExtractor= {item=>item.id.toString()}
+
+                />
+
             </View>
         )
-    }
+
+    } 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff"
+     },
+    loader:{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#fff"
+     },
+    list:{
+      paddingVertical: 4,
+      margin: 5,
+      backgroundColor: "#fff"
+     }
+  });
